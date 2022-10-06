211d8b90eea8b67ba4f1ed4650d85afc827b05e8
atharellahi <official.atharellahi@gmail.com>
2022-10-06 22:46:17 +0530

    updated

--- kgcweb.js
+++ kgcweb.js
@@ -2,9 +2,20 @@
 const player = document.querySelector('.youtube-player')
 const ytItems = document.querySelectorAll('.thumb');
 for (ytItem of ytItems) {
-ytItem.addEventListener('click',(e) => {
-    const videoSource = e.target.getAttribute('mysrc')
-    player.innerHTML = `<iframe src=${videoSource} title="YouTube video player"
+    ytItem.addEventListener('click', (e) => {
+        const videoSource = e.target.getAttribute('mysrc')
+        player.innerHTML = `<iframe src=${videoSource} title="YouTube video player"
 allowfullscreen></iframe>`
-})
+    })
 }
+
+const mnubtn = document.querySelector('#menubtn');
+const navdrop = document.querySelector('#dropdown');
+mnubtn.addEventListener('click', () => {
+    if (navdrop.classList.value === 'hidden') {
+        navdrop.classList.remove('hidden');
+    }
+    else {
+        navdrop.classList.add('hidden');
+    }
+})
